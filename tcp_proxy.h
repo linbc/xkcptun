f6506cf2e2a2f3b8adb6f0d61052044cde3cd086
linbc <linbc01@gmail.com>
2019-02-10 17:26:57 +0800

    move to src

--- tcp_proxy.h
+++
@@ -1,9 +0,0 @@
-#ifndef	_TCP_PROXY_
-#define	_TCP_PROXY_
-
-#include "ikcp.h"
-
-void tcp_proxy_accept_cb(struct evconnlistener *listener, evutil_socket_t fd,
-    					 struct sockaddr *a, int slen, void *param);
-
-#endif
